642bcf9a968add3807932f660c3e06f24241492e
Skips <billcollins759@gmail.com>
2019-07-01 22:10:32 -0400

    productos corregido

--- app/src/main/java/com/example/herald/proyecto/ProductosActivity.java
+++ app/src/main/java/com/example/herald/proyecto/ProductosActivity.java
@@ -1,13 +1,44 @@
 package com.example.herald.proyecto;
 
-import android.support.v7.app.AppCompatActivity;
+
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import java.util.ArrayList;
+import java.util.Arrays;
+
 
 public class ProductosActivity extends AppCompatActivity {
-
+    ArrayAdapter<String> adapter;
+    EditText editText;
+    ArrayList<String> itemList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_productos);
+        String[] items={"Auto en venta","Regalo cachorros"};
+        itemList=new ArrayList<String>(Arrays.asList(items));
+        adapter=new ArrayAdapter<String>(this,R.layout.list_item,R.id.txtview,itemList);
+        ListView listV=(ListView)findViewById(R.id.list);
+        listV.setAdapter(adapter);
+        editText=(EditText)findViewById(R.id.txtInput);
+        Button btAdd=(Button)findViewById(R.id.btAdd);
+        btAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String newItem=editText.getText().toString();
+
+                itemList.add(newItem);
+
+                adapter.notifyDataSetChanged();
+            }
+
+        });
+
     }
+
 }
