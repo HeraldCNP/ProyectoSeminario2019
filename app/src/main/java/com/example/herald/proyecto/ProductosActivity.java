6acfd133b1acf88adc21124a38a92d1ec7a8acc0
Skips <billcollins759@gmail.com>
2019-07-04 13:38:37 -0400

    productos + db

--- app/src/main/java/com/example/herald/proyecto/ProductosActivity.java
+++ app/src/main/java/com/example/herald/proyecto/ProductosActivity.java
@@ -1,44 +1,88 @@
 package com.example.herald.proyecto;
 
+import cz.msebera.android.httpclient.Header;
+import com.loopj.android.http.*;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
-import java.util.ArrayList;
-import java.util.Arrays;
+import android.widget.SearchView;
+import android.widget.Spinner;
+import android.widget.Toast;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
 
 public class ProductosActivity extends AppCompatActivity {
-    ArrayAdapter<String> adapter;
-    EditText editText;
-    ArrayList<String> itemList;
+    ArrayList<String> categoria;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_productos);
-        String[] items={"Auto en venta","Regalo cachorros"};
-        itemList=new ArrayList<String>(Arrays.asList(items));
-        adapter=new ArrayAdapter<String>(this,R.layout.list_item,R.id.txtview,itemList);
-        ListView listV=(ListView)findViewById(R.id.list);
-        listV.setAdapter(adapter);
-        editText=(EditText)findViewById(R.id.txtInput);
-        Button btAdd=(Button)findViewById(R.id.btAdd);
-        btAdd.setOnClickListener(new View.OnClickListener() {
+        setSpinner();
+        Button btn = findViewById(R.id.btnpr);
+        btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String newItem=editText.getText().toString();
-
-                itemList.add(newItem);
-
-                adapter.notifyDataSetChanged();
+                Snackbar.make(v, "Guardando datos", Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show();
+                sendData();
             }
+        });
+
+    }
+    private void sendData() {
+        EditText txtname = findViewById(R.id.name);
+        EditText txtprice = findViewById(R.id.price);
+        Spinner spcategory = findViewById(R.id.category);
+        EditText txtdescription = findViewById(R.id.description);
+        EditText txtstock = findViewById(R.id.stock);
+
+        AsyncHttpClient client = new AsyncHttpClient();
+        RequestParams params = new RequestParams();
+        params.add("name", txtname.getText().toString());
+        params.add("price", txtprice.getText().toString());
+        params.add("category", categoria.get(spcategory.getSelectedItemPosition()));
+        params.add("description", txtdescription.getText().toString());
+        params.add("stock", txtstock.getText().toString());
+
+        client.post(Utils.PRODUCTS_SERVICE, params, new JsonHttpResponseHandler() {
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                if(response.has("price")){
+                    Toast.makeText(ProductosActivity.this, "Datos enviados", Toast.LENGTH_LONG);
+                    Intent main = new Intent(ProductosActivity.this, MainActivity.class);
+                    startActivity(main);
+                }
+            }
+
+            public void onSuccess(int statusCode, Header[] headers, JSONArray response) {
+
+            }
+
+            /*public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
+
+            }*/
 
         });
 
     }
-
+    private void setSpinner() {
+        categoria = new ArrayList<>();
+        categoria.add("Hogar");
+        categoria.add("Entretenimiento");
+        categoria.add("Comercio");
+        categoria.add("Electronica");
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, categoria);
+        Spinner spinner = findViewById(R.id.category);
+        spinner.setAdapter(adapter);
+    }
 }
---
+++ app/src/main/java/com/example/herald/proyecto/Utils.java
@@ -0,0 +1,10 @@
+package com.example.herald.proyecto;
+
+public class Utils {
+    public static String HOST = "http://192.168.1.152:8000";
+    public static String USER_SERVICE = HOST + "/v1.0/ventas/user";
+    public static String PRODUCTS_SERVICE = HOST + "/v1.0/ventas/product";
+}
+
+
+
