4127a8fe56a314fbac6633e466966d6d04d1a067
Skips <billcollins759@gmail.com>
2019-07-06 01:44:54 -0400

    probando imagen

--- app/src/main/java/com/example/herald/proyecto/ProductosActivity.java
+++ app/src/main/java/com/example/herald/proyecto/ProductosActivity.java
@@ -3,30 +3,87 @@ package com.example.herald.proyecto;
 import cz.msebera.android.httpclient.Header;
 import com.loopj.android.http.*;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.annotation.RequiresApi;
 import android.support.design.widget.Snackbar;
+import android.support.v4.content.FileProvider;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.SearchView;
 import android.widget.Spinner;
 import android.widget.Toast;
 
 import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 
 public class ProductosActivity extends AppCompatActivity {
     ArrayList<String> categoria;
 
+    //imagen
+    GridLayoutManager lay;
+    RecyclerView recycler;
+    menuAdapter men;
+    Button cargar,insert;
+    ImageView img;
+    private final int CODE_PERMISSIONS = 101;
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_productos);
+
+        //img
+
+        /*recycler=findViewById(R.id.recyclerId);
+        lay=new GridLayoutManager(this,2);
+        recycler.setLayoutManager(lay);
+        men=new menuAdapter(this);
+        cargar();
+        recycler.setAdapter(men);
+
+        cargar=findViewById(R.id.cargarImagen);
+        cargar.setOnClickListener((View.OnClickListener) this);
+        insert=findViewById(R.id.insertarImagen);
+
+        insert.setVisibility(View.INVISIBLE);
+        if(reviewPermissions()){
+            insert.setVisibility(View.VISIBLE);
+        }
+        insert.setOnClickListener((View.OnClickListener) this);
+
+        img=findViewById(R.id.vistaImagenInsertar);*/
+
+        //finimg
+
         setContentView(R.layout.activity_productos);
         setSpinner();
         Button btn = findViewById(R.id.btnpr);
@@ -40,6 +97,27 @@ public class ProductosActivity extends AppCompatActivity {
         });
 
     }
+
+    //img
+  /*  private void cargar() {
+        AsyncHttpClient client=new AsyncHttpClient();
+        client.get(Utils.localhost+"image",null,new JsonHttpResponseHandler(){
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONArray response) {
+                super.onSuccess(statusCode, headers, response);
+                for(int i=0;i<response.length();i++){
+                    try {
+                        JSONObject dat=response.getJSONObject(i);
+                        String url=dat.getString("url");
+                        men.add(new item(url));
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }*/
+
+
+
     private void sendData() {
         EditText txtname = findViewById(R.id.name);
         EditText txtprice = findViewById(R.id.price);
@@ -58,7 +136,7 @@ public class ProductosActivity extends AppCompatActivity {
         client.post(Utils.PRODUCTS_SERVICE, params, new JsonHttpResponseHandler() {
             public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
                 if(response.has("price")){
-                    Toast.makeText(ProductosActivity.this, "Datos enviados", Toast.LENGTH_LONG);
+                    Toast.makeText(ProductosActivity.this, "Datos enviados", Toast.LENGTH_LONG).show();
                     Intent main = new Intent(ProductosActivity.this, MainActivity.class);
                     startActivity(main);
                 }
@@ -85,4 +163,5 @@ public class ProductosActivity extends AppCompatActivity {
         Spinner spinner = findViewById(R.id.category);
         spinner.setAdapter(adapter);
     }
+
 }
---
+++ app/src/main/java/com/example/herald/proyecto/item.java
@@ -0,0 +1,12 @@
+package com.example.herald.proyecto;
+
+public class item {
+    private String url;
+    item(String u){
+        url=u;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+}
