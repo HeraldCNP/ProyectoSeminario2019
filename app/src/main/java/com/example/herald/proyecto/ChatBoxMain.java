826d7800a0fa23cd3ac2b474cc60c00264bf6594
Skips <billcollins759@gmail.com>
2019-07-14 13:39:03 -0400

    adaptador chat

--- app/src/main/java/com/example/herald/proyecto/ChatBoxMain.java
+++ app/src/main/java/com/example/herald/proyecto/ChatBoxMain.java
@@ -1,8 +1,7 @@
 package com.example.herald.proyecto;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.content.Intent;
+import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
